ebf44b0b12113cd1c4b77852b66be925c9da720c
kaylommy <kaylajfreeman13@gmail.com>
2024-08-28 20:03:39 -0400

    added code for play interaction

--- index.js
+++ index.js
@@ -54,3 +54,91 @@ player.on("channelEmpty", (queue) => {
 player.on("queueEnd", (queue) => {
     queue.metadata.send("✅ | Queue finished!");
 });
+
+// event listener for when a message is created in the channel the bot has access to.
+client.on("messageCreate", async (message) => {
+    // if the author is a bot or not in the server return
+    if (message.author.bot || !message.guild) 
+        return;
+    // if the application owner is not already fetched it will fetch the details
+    if (!client.application?.owner) 
+        await client.application?.fetch();
+    
+    if (message.content === "!deploy" && message.author.id === client.application?.owner?.id) {
+        // set up of slash commands
+        await message.guild.commands.set([
+            {
+                name: "play",
+                description: "Plays a song from youtube",
+                options: [
+                    {
+                        name: "query",
+                        type: "STRING",
+                        description: "The song you want to play",
+                        required: true
+                    }
+                ]
+            },
+            {
+                name: "skip",
+                description: "Skip to the current song"
+            },
+            {
+                name: "queue",
+                description: "See the queue"
+            },
+            {
+                name: "stop",
+                description: "Stop the player"
+            },
+        ]);
+    
+        await message.reply("Deployed!");
+    }
+});
+
+// triggered when an interaction is created
+client.on("interactionCreate", async (interaction) => {
+    // checks for the interaction being a command or if it occurs in the discord server
+    if (!interaction.isCommand() || !interaction.guildId) 
+        return;
+    // checks if the member is an instance of server member or if they are in a voice channel
+    if (!(interaction.member instanceof GuildMember) || !interaction.member.voice.channel) {
+        return void interaction.reply({ content: "You are not in a voice channel!", ephemeral: true });
+    }
+    // if the user is in a voice channel different from the bot
+    if (interaction.guild.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.me.voice.channelId) {
+        return void interaction.reply({ content: "You are not in my voice channel!", ephemeral: true });
+    }
+    // if commant is play then handle command
+    if (interaction.commandName === "play") {
+        await interaction.deferReply();
+        // gets the value of "query"
+        const query = interaction.options.get("query").value;
+        const searchResult = await Player
+            .search(query, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.AUTO
+            })
+            // error handler for no results
+            .catch(() => {});
+            if(!searchResult || !searchResult.tracks.length) 
+                return void interaction.followUp({ content: "No results were found!" })
+            
+            // queue creation
+            const queue = await player.createQueue(interaction.guild, {
+            metadata: interaction.channel // will contain inforamtion about the channel (queue info)
+            });
+        
+            try {
+                if (!queue.connection) await queue.connect(interaction.member.voice.channel);
+            } catch {
+                void player.deleteQueue(interaction.guildId);
+                return void interaction.followUp({ content: "Could not join your voice channel!" });
+            }
+        
+            await interaction.followUp({ content: `⏱ | Loading your ${searchResult.playlist ? "playlist" : "track"}...` });
+            searchResult.playlist ? queue.addTracks(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
+            if (!queue.playing) await queue.play();
+    }
+});
